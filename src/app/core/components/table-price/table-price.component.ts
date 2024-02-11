0a169acc1b4d8e9ab1e0580aff17d48d4a4897f0
Angel Gomez <106106151+AngelGomez12@users.noreply.github.com>
2024-02-10 21:05:27 -0300

    feat: :sparkles: Refactor price table component and add price form component

--- src/app/core/components/table-price/table-price.component.ts
+++ src/app/core/components/table-price/table-price.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-table-price',
   standalone: true,
   imports: [],
   templateUrl: './table-price.component.html',
-  styleUrl: './table-price.component.css'
+  styleUrl: './table-price.component.css',
 })
 export class TablePriceComponent {
-
+  @Input() table: { columns: string[]; rows: string[][] } = {
+    columns: [],
+    rows: [],
+  };
 }
--- src/app/features/settings/pages/settings-pages/settings-pages.component.ts
+++ src/app/features/settings/pages/settings-pages/settings-pages.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { PriceFormComponent } from '../../components/price-form/price-form.component';
 
 @Component({
   selector: 'app-settings-pages',
   standalone: true,
-  imports: [],
   templateUrl: './settings-pages.component.html',
-  styleUrl: './settings-pages.component.css'
+  styleUrl: './settings-pages.component.css',
+  imports: [PriceFormComponent],
 })
-export class SettingsPagesComponent {
-
-}
+export class SettingsPagesComponent {}
