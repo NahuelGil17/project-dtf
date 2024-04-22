5ddaec9767f82393929628f30f0c405db1577784
Nico Gilardoni <nicogilardonik@gmail.com>
2024-04-22 19:25:01 -0300

    remove comments

--- src/app/features/landing/services/contact-form.service.ts
+++ src/app/features/landing/services/contact-form.service.ts
@@ -8,16 +8,12 @@ export class ContactFormService {
 
   constructor() { }
 
-  // Método para enviar el formulario de contacto
   async sendContactForm(name: string, email: string, message: string) {
     try {
-      // Obtenemos una instancia de las funciones de Firebase
       const functions = getFunctions();
 
-      // Creamos una referencia a la función HTTPS llamada "sendMail"
       const sendMail = httpsCallable(functions, 'sendMail');
 
-      // Llamamos a la función HTTPS "sendMail" con los parámetros necesarios
       const result = await sendMail({ 
         to: 'nicogilardonik@gmail.com', 
         message: {
@@ -26,13 +22,11 @@ export class ContactFormService {
         }
       });
 
-      // Si el correo se envió con éxito, mostramos un mensaje en la consola
       console.log('Email sent successfully:', result);
 
-      // Devolvemos el resultado (puede ser útil para manejar la respuesta en otros lugares)
       return result;
     } catch (error) {
-      // Si ocurre un error al enviar el correo, lo mostramos en la consola y lanzamos una excepción
+
       console.error('Error sending email:', error);
       throw error;
     }
