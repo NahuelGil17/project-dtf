2392123ca926ee7450e3f9c273054a6267d7707b
Nico Gilardoni <nicogilardonik@gmail.com>
2024-04-26 13:43:17 -0300

    feat: :sparkles: Update contact-page.component.ts to incude OnInit interface and add success notification on form submissionl

--- src/app/features/landing/pages/contact-page/contact-page.component.ts
+++ src/app/features/landing/pages/contact-page/contact-page.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ContactFormComponent } from '../../components/contact-form/contact-form.component';
-import { Store } from '@ngxs/store';
+import { Actions, Store, ofActionSuccessful } from '@ngxs/store';
 import { SendContactForm } from '../../state/contact.actions';
 import { ContactEmail } from '../../interfaces/contactemail.interface';
+import { tap } from 'rxjs';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-contact-page',
   standalone: true,
@@ -12,10 +14,30 @@ import { ContactEmail } from '../../interfaces/contactemail.interface';
   styleUrls: ['./contact-page.component.css'],
   imports: [ReactiveFormsModule, CommonModule, ContactFormComponent],
 })
-export class ContactPageComponent {
-  constructor(private store: Store) {}
+export class ContactPageComponent implements OnInit{
+  constructor(private store: Store, private actions: Actions) {}
+
+  ngOnInit(): void {
+  
+    this.actions
+      .pipe(
+        ofActionSuccessful(SendContactForm),
+        tap(() => {
+          debugger
+          Swal.fire({
+            title: 'Enviado!',
+            text: 'Email enviado correctamente',
+            icon: 'success',
+          });
+        })
+      )
+      .subscribe(() => {});
+
+
+  }
 
   handleContactForm(contactEmail: ContactEmail) {  
     this.store.dispatch(new SendContactForm(contactEmail));
+    
   }
 }
