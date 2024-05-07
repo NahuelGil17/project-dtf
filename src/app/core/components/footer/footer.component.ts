de0b8494324c729c08ffb1a82c69ef13c66c7817
Nico Gilardoni <nicogilardonik@gmail.com>
2024-05-07 10:15:01 -0300

    feat: :sparkles: Update footer component with dynamic WhatsApp link

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -4,6 +4,7 @@ import { AuthState } from '../../../features/auth/state/auth.state';
 import { Select } from '@ngxs/store';
 import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
+import { environment } from '../../../environment/environment.develop';
 
 @Component({
   selector: 'app-footer',
@@ -15,6 +16,7 @@ import { CommonModule } from '@angular/common';
 export class FooterComponent implements OnInit {
   @Select(AuthState.isAdmin) isAdmin$!: Observable<boolean>;
   currentYear: number = 2024;
+  phone: number = environment.PHONE
   
 
   constructor() {}
